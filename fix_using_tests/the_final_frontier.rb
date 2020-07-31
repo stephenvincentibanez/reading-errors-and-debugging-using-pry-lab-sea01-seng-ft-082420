50e20b124eac0818cff53a0a550662e15a424c83
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-07-31 20:57:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -9,6 +10,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+binding.pry
 def engage
   puts state_log(date)
   date = generate_star_date
